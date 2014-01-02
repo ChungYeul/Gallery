69a04e2a15cc85c7154016eeee6f57d4da03d0f3
SDT-1 <sdt-1@C3-11ui-MacBook-Pro.local>
2014-01-02 16:12:16 +0900

    Initial Commit

---
+++ GalleryTests/GalleryTests.m
@@ -0,0 +1,34 @@
+//
+//  GalleryTests.m
+//  GalleryTests
+//
+//  Created by SDT-1 on 2014. 1. 2..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GalleryTests : XCTestCase
+
+@end
+
+@implementation GalleryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
